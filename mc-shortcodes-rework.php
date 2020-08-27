91ed9cfa9a9817f731141ee8bc5c83c8a21eb914
przemekcichon <przemekcichon@gmail.com>
2020-08-27 21:49:41 +0200

    Update mc-shortcodes-rework.php

--- mc-shortcodes-rework.php
+++ mc-shortcodes-rework.php
@@ -3,4 +3,65 @@
 Plugin Name: MC Shortcodes Rework
 */ 
 
+// Service Highlight =========================================================================== >	
+if( !function_exists( 'mediaconsult_service_highlight_rework' ) ) {
+	function mediaconsult_service_highlight_rework( $atts, $content = null ) {
+		extract( shortcode_atts( array(
+			'class'			=> '',
+			'title'			=> '',
+			'icon'			=> '',
+			'icon_type'		=> '',
+			'background'	=> '',
+      'url'     => '',
+      'link'    => ''
+		  ),
+		  $atts ) );
+		
+		$mediaconsult_url = '';
+    
+    if ( 1 == $link ) {
+      $mediaconsult_url = ' href="' . esc_url( $url ) . '"';
+    }
+		
+    $mediaconsult_background = '';
+		
+		if ( $background ) {
+			$mediaconsult_background = ' style="background-color: ' . esc_attr( $background ) . '; "';
+		}
+		
+		
+		$output = '';
+		
+      $output .= '<a ' . $mediaconsult_url . ' class="celestial-button-fill celestial-button-skin" >';
+		
+        $output .= '<div class="cel-service-highlight ' .  esc_attr( $class ) . '">';
 
+
+          $output .= '<div class="serh-title-wrapper" ' . $mediaconsult_background . '>';
+
+            if( $icon ) {
+              if( $icon_type == 'icon' ) {
+                $output .= '<i class="serh-icon ' . esc_attr( $icon ) . '"></i>';
+              } else {
+                $output .= '<img src="' . esc_url( $icon ) . '" class="serh-icon" alt="' . esc_attr( $title ) . '" />';
+              }
+            }
+            if( $title ) {
+              $output .= '<h4 class="serh-title">' . esc_html( $title ) . '</h4>';
+            }
+
+          $output .= '</div>';
+
+
+          $output .= '<div class="serh-content">' . do_shortcode( $content ) . '</div>';
+
+        $output .= '</div>';
+    
+    $output .= '</a>';
+		
+		
+		return $output;
+	 
+	}
+	add_shortcode( 'service_highlight_rework', 'mediaconsult_service_highlight_rework' );
+} 
